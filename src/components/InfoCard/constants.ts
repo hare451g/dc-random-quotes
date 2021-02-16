69a5bd9479413fd280b70f8de2e30ad0ffffd35a
hendra sadewa <hendrasadewa30@gmail.com>
2021-02-16 20:09:18 +0700

    create infocard component

---
+++ src/components/InfoCard/constants.ts
@@ -0,0 +1,9 @@
+const constants = {
+  messages: {
+    IS_LOADING: 'Loading contents . . .',
+    AUTHOR_IS_NULL: 'Unknown author',
+    GENRE_IS_NULL: 'Unknown genre',
+  },
+};
+
+export default constants;
